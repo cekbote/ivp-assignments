cd791b364a7a7f6c273fa741f249d29185c580e9
Chanakya-Ekbote <ca10@iitbbs.ac.in>
2020-09-19 23:18:59 +0530

    Added histogram_equalisation.
    Signed-off-by: Chanakya-Ekbote <ca10@iitbbs.ac.in>

---
+++ Assignment-1/histogram_equalization.m
@@ -0,0 +1,53 @@
+function [histeqimage] = histogram_equalization(image)
+    [row, col] = size(image);
+    keys = [];
+    histeqimage = image;
+    
+    hist_map = containers.Map();
+    cdf_map = containers.Map();
+    hist_eq_map = containers.Map();
+    
+    for i=1:row
+        for j=1:col
+            key = char(image(i, j));
+            if isKey(hist_map, key)
+                hist_map(key) = hist_map(key) + 1;
+            else
+                hist_map(key) = 1;
+                keys = [keys; key]; 
+            end
+        end
+    end
+    
+    keys = sort(keys);
+    sum = 0;
+    cdf_min = hist_map(keys(0));
+    
+    for i=1:keys
+        sum = sum + hist_map(keys(i));
+        cdf_map(keys(i)) = sum;
+    end
+    
+    for i=1:keys
+        hist_eq_map(keys(i)) = round((cdf_map(keys(i))-cdf_min)*255/(row*col-cdf_min));
+    end
+    
+    for i=1:row
+        for j=1:col
+            key = char(image(i, j));
+            histeqimage(i,j) = hist_eq_map(key);
+        end
+    end
+    
+    
+    
+    
+    
+    
+    
+    
+            
+            
+            
+            
+    
